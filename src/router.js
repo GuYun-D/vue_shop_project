2196e98619ac8ff83e1073286488242bfc55a683
孤云 <1946306647@qq.com>
2021-03-03 14:56:24 +0800

    实现路由首页重定向

--- src/router.js
+++ src/router.js
@@ -4,28 +4,36 @@ import Router from 'vue-router'
 import Login from './components/login.vue'
 import Home from './components/home.vue'
 
+import Welcome from './components/welcome.vue'
+
 Vue.use(Router)
 
-const router =  new Router({
+const router = new Router({
   routes: [
-    {path: '/', redirect: '/login'},
-    {path: '/login', component: Login},
-    {path: '/home', component: Home}
+    { path: '/', redirect: '/login' },
+    { path: '/login', component: Login },
+    {
+      path: '/home', component: Home,
+      redirect: '/welcome',
+      children: [
+        { path: '/welcome', component: Welcome }
+      ]
+    }
   ]
 })
 
 // 挂载路由首位
 router.beforeEach((to, from, next) => {
-  if(to.path == '/login'){
+  if (to.path == '/login') {
     return next()
   }
   // 获取token
   const tokenStr = window.sessionStorage.getItem("token")
-  if(!tokenStr){
+  if (!tokenStr) {
     // 用户未登录
     // 强制跳转到登录页
     return next('/login')
-  }else{
+  } else {
     next()
   }
 })
