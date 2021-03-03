1f110ccc307be8f77fa3b1b30f99dc492089c81b
孤云 <1946306647@qq.com>
2021-03-03 15:45:19 +0800

    绘制用户列表的基础样式

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -1,15 +1,39 @@
 <template>
-    <div>
-        <h3>用户列表</h3>
-    </div>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片试图区域 -->
+    <el-card>
+      <!-- 使用layout布局 -->
+      <!-- 
+          gutter:	栅格间隔
+          span: 栅格占据的列数
+       -->
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <!-- 搜索与添加区域 -->
+          <div>
+            <el-input placeholder="请输入内容">
+              <el-button slot="append" icon="el-icon-search"></el-button>
+            </el-input>
+          </div>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary">添加用户</el-button>
+        </el-col>
+      </el-row>
+    </el-card>
+  </div>
 </template>
 
 <script>
-export default {
-    
-}
+export default {}
 </script>
 
 <style scoped>
-
 </style>
