21c0cc16d61330df1d6db238e18f07682170af7c
孤云 <1946306647@qq.com>
2021-03-06 12:04:14 +0800

    渲染数据

--- src/components/power/rights.vue
+++ src/components/power/rights.vue
@@ -8,7 +8,21 @@
     </el-breadcrumb>
 
     <!-- 视图区域 -->
-    <el-card> </el-card>
+    <el-card>
+      <el-table :data="rightsList" border stripe>
+        <el-table-column type="index"> </el-table-column>
+        <el-table-column label="权限名称" prop="authName"> </el-table-column>
+        <el-table-column label="路径" prop="path"> </el-table-column>
+        <el-table-column label="权限等级" prop="level"> 
+            <template slot-scope="scope">
+            <el-tag v-if="scope.row.level === '0'">一级</el-tag>
+            <el-tag type="success" v-else-if="scope.row.level === '1'">二级</el-tag>
+            <el-tag type="warning" v-else>三级</el-tag>
+          </template>
+        </el-table-column>
+
+      </el-table>
+    </el-card>
   </div>
 </template>
 
@@ -35,11 +49,11 @@ export default {
         return this.$message.error('权限列表获取失败')
       }
 
-      this.$message.success("权限数据列表获取成功")
+      this.$message.success('权限数据列表获取成功')
 
       this.rightsList = res.data
 
-      console.log(this.rightsList);
+      console.log(this.rightsList)
     },
   },
 }
