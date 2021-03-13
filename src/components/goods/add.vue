c8ddb9c47fc2bc4236e46c9979e724f1e15e774f
孤云 <1946306647@qq.com>
2021-03-13 20:05:41 +0800

    渲染tab栏

--- src/components/goods/add.vue
+++ src/components/goods/add.vue
@@ -19,7 +19,12 @@
     </el-card>
 
     <!-- 步骤条区域 -->
-    <el-steps :space="200" :active="activeIndex" finish-status="success" align-center>
+    <el-steps
+      :space="200"
+      :active="activeIndex"
+      finish-status="success"
+      align-center
+    >
       <el-step title="基本信息"></el-step>
       <el-step title="商品参数"></el-step>
       <el-step title="商品属性"></el-step>
@@ -27,6 +32,18 @@
       <el-step title="商品内容"></el-step>
       <el-step title="完成"></el-step>
     </el-steps>
+
+    <!-- tab栏区域 -->
+    <!-- 
+        tab-position:tab栏所在区域
+     -->
+    <el-tabs :tab-position="'left'" style="height: 200px">
+      <el-tab-pane label="基本信息">基本信息</el-tab-pane>
+      <el-tab-pane label="商品参数">商品参数</el-tab-pane>
+      <el-tab-pane label="商品属性">商品属性</el-tab-pane>
+      <el-tab-pane label="商品图片">商品图片</el-tab-pane>
+      <el-tab-pane label="商品内容">商品内容</el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
