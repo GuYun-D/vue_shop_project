7dc50adeb0305a2146d884ade6a17a769f870bdd
孤云 <1946306647@qq.com>
2021-03-13 20:00:14 +0800

    使用步骤条

--- src/components/goods/add.vue
+++ src/components/goods/add.vue
@@ -1,27 +1,48 @@
 <template>
-    <div>
-        
-    </div>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>添加商品</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <el-alert
+        title="添加商品信息"
+        type="info"
+        center
+        show-icon
+        :closable="false"
+      ></el-alert>
+    </el-card>
+
+    <!-- 步骤条区域 -->
+    <el-steps :space="200" :active="activeIndex" finish-status="success" align-center>
+      <el-step title="基本信息"></el-step>
+      <el-step title="商品参数"></el-step>
+      <el-step title="商品属性"></el-step>
+      <el-step title="商品图片"></el-step>
+      <el-step title="商品内容"></el-step>
+      <el-step title="完成"></el-step>
+    </el-steps>
+  </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return {
-
-        }
-    },
-
-    created(){
-
-    },
-
-    methods(){
-
+  data() {
+    return {
+      activeIndex: 0,
     }
+  },
+
+  created() {},
+
+  methods() {},
 }
 </script>
 
 <style scoped>
-
 </style>
