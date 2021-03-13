abc87fb6764460ec0373dde1ef8d51e98627f2b0
孤云 <1946306647@qq.com>
2021-03-13 15:24:12 +0800

    獲取商品列表數據

--- src/components/goods/list.vue
+++ src/components/goods/list.vue
@@ -11,14 +11,12 @@
     <el-card>
       <el-row :gutter="20">
         <el-col :span="8">
-          <el-input
-            placeholder="请输入内容"
-          >
+          <el-input placeholder="请输入内容">
             <el-button slot="append" icon="el-icon-search"></el-button>
           </el-input>
         </el-col>
-        <el-col :span="4" >
-            <el-button type="primary">添加商品</el-button>
+        <el-col :span="4">
+          <el-button type="primary">添加商品</el-button>
         </el-col>
       </el-row>
     </el-card>
@@ -28,12 +26,51 @@
 <script>
 export default {
   data() {
-    return {}
+    return {
+      // 查询参数对象
+      queryInfo: {
+        // 搜索关键字
+        query: '',
+        // 当前页码
+        pagenum: 1,
+        // 每页显示条数
+        pagesize: 10,
+      },
+
+      // 商品列表數據
+      goodslist: [],
+
+      // 縂數據條數
+      total: 0
+    }
   },
 
-  created() {},
+  created() {
+    // 获取商品列表数据
+    this.getGoodsList()
+  },
 
-  methods: {},
+  methods: {
+    // 根据分页获取商品列表数据
+    async getGoodsList() {
+      const { data: res } = await this.$http.get('goods', {
+        params: this.queryInfo,
+      })
+
+      console.log('商品列表数据')
+      console.log(res)
+
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取商品列表失败')
+      }
+
+      this.$message.success('获取商品列表成功')
+
+      this.goodslist = res.data.goods
+
+      this.total = res.data.total
+    },
+  },
 }
 </script>
 
