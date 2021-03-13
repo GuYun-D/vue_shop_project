87dd38fa403df7f227743daaecaeb23a8b8272b8
孤云 <1946306647@qq.com>
2021-03-13 16:56:07 +0800

    实现商品列表的分页功能

--- src/components/goods/list.vue
+++ src/components/goods/list.vue
@@ -26,11 +26,19 @@
         <el-table-column type="index"></el-table-column>
         <!--  -->
         <el-table-column label="商品名称" prop="goods_name"></el-table-column>
-        <el-table-column label="商品价格" prop="goods_price" width="95px"></el-table-column>
-        <el-table-column label="商品重量" prop="goods_weight" width="70px"></el-table-column>
+        <el-table-column
+          label="商品价格"
+          prop="goods_price"
+          width="95px"
+        ></el-table-column>
+        <el-table-column
+          label="商品重量"
+          prop="goods_weight"
+          width="70px"
+        ></el-table-column>
         <el-table-column label="创建时间" prop="add_time" width="140px">
           <template slot-scope="scope">
-            {{scope.row.add_time | dateFormat}}
+            {{ scope.row.add_time | dateFormat }}
           </template>
         </el-table-column>
         <el-table-column label="操作" width="130px">
@@ -48,6 +56,19 @@
           </template>
         </el-table-column>
       </el-table>
+
+      <!-- 分页区域 -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[5, 10, 15, 20]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+        background
+      >
+      </el-pagination>
     </el-card>
   </div>
 </template>
@@ -99,9 +120,22 @@ export default {
 
       this.total = res.data.total
     },
+
+    handleSizeChange(newSize){
+      this.queryInfo.pagesize = newSize
+      this.getGoodsList()
+    },
+
+    handleCurrentChange(newPage){
+      this.queryInfo.pagenum = newPage
+      this.getGoodsList()
+    }
   },
 }
 </script>
 
 <style scoped>
+  .el-pagination{
+    margin-top: 40px;
+  }
 </style>
