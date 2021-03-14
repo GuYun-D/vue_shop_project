36a4557eacd06e4897a6a02b908c5851aadd0c5e
孤云 <1946306647@qq.com>
2021-03-14 10:06:34 +0800

    绘制基本信息的ui解构

--- src/components/goods/add.vue
+++ src/components/goods/add.vue
@@ -16,57 +16,70 @@
         show-icon
         :closable="false"
       ></el-alert>
-    </el-card>
 
-    <!-- 步骤条区域 -->
-    <el-steps
-      :space="200"
-      :active="activeIndex - 0"
-      finish-status="success"
-      align-center
-    >
-      <el-step title="基本信息"></el-step>
-      <el-step title="商品参数"></el-step>
-      <el-step title="商品属性"></el-step>
-      <el-step title="商品图片"></el-step>
-      <el-step title="商品内容"></el-step>
-      <el-step title="完成"></el-step>
-    </el-steps>
+      <!-- 步骤条区域 -->
+      <el-steps
+        :space="200"
+        :active="activeIndex - 0"
+        finish-status="success"
+        align-center
+      >
+        <el-step title="基本信息"></el-step>
+        <el-step title="商品参数"></el-step>
+        <el-step title="商品属性"></el-step>
+        <el-step title="商品图片"></el-step>
+        <el-step title="商品内容"></el-step>
+        <el-step title="完成"></el-step>
+      </el-steps>
 
-    <!-- tab栏区域 -->
-    <!-- 
+      <!-- tab栏区域 -->
+      <!-- 
         tab-position:tab栏所在区域
      -->
-    <!-- 
+      <!-- 
         tab栏与步骤条数据联动
         步骤条接收number类型，activeIndex - 0转为数字
         tab栏接受string类型
      -->
-    <!-- 
+      <!-- 
          labe-position：label位置
       -->
-    <!-- 
+      <!-- 
           五个tab栏的表单要同时提交，所以在五个的最外层添加表单控件
        -->
-    <el-form
-      :model="addForm"
-      :rules="addFormRules"
-      ref="ruleFormRef"
-      label-width="100px"
-      labe-position="top"
-    >
-      <el-tabs
-        v-model="activeIndex"
-        :tab-position="'left'"
-        style="height: 200px"
+      <el-form
+        :model="addForm"
+        :rules="addFormRules"
+        ref="ruleFormRef"
+        label-width="100px"
+        label-position="top"
       >
-        <el-tab-pane label="基本信息" name="0">基本信息</el-tab-pane>
-        <el-tab-pane label="商品参数" name="1">商品参数</el-tab-pane>
-        <el-tab-pane label="商品属性" name="2">商品属性</el-tab-pane>
-        <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
-        <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
-      </el-tabs>
-    </el-form>
+        <el-tabs
+          v-model="activeIndex"
+          :tab-position="'left'"
+          style="height: 200px"
+        >
+          <el-tab-pane label="基本信息" name="0">
+            <el-form-item label="商品名称" prop="goods_name">
+              <el-input v-model="addForm.goods_name"></el-input>
+            </el-form-item>
+            <el-form-item label="商品价格" prop="goods_price">
+              <el-input v-model="addForm.goods_price" type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品重量" prop="goods_weight">
+              <el-input v-model="addForm.goods_weight"></el-input>
+            </el-form-item>
+            <el-form-item label="商品数量" prop="goods_number">
+              <el-input v-model="addForm.goods_number" type="number"></el-input>
+            </el-form-item>
+          </el-tab-pane>
+          <el-tab-pane label="商品参数" name="1">商品参数</el-tab-pane>
+          <el-tab-pane label="商品属性" name="2">商品属性</el-tab-pane>
+          <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
+          <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
+        </el-tabs>
+      </el-form>
+    </el-card>
   </div>
 </template>
 
@@ -77,10 +90,49 @@ export default {
       activeIndex: '0',
 
       // 添加商品的表单数据对象
-      addForm: {},
+      addForm: {
+        goods_name: '',
+        goods_price: 0,
+        goods_weight: 0,
+        goods_number: 0
+      },
 
       // 表单的验证规则
-      addFormRules: {},
+      addFormRules: {
+        goods_name: [
+          {
+            required: true,
+            message: '请输入商品名称',
+            trigger: 'blur',
+          },
+        ],
+
+        goods_price: [
+          {
+            required: true,
+            message: '请输入商品价格',
+            trigger: 'blur',
+          },
+        ],
+
+        goods_weight: [
+          {
+            required: true,
+            message: '请输入商品重量',
+            trigger: 'blur',
+          },
+        ],
+
+        goods_number: [
+          {
+            required: true,
+            message: '请输入商品数量',
+            trigger: 'blur',
+          },
+        ],
+      },
+
+      //
     }
   },
 
