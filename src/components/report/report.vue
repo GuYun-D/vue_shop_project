797b7ab1a729ab51431792c91c177b70c2d77676
孤云 <1946306647@qq.com>
2021-03-16 17:01:44 +0800

    bug修改以及渲染数据报表页面

--- src/components/report/report.vue
+++ src/components/report/report.vue
@@ -7,13 +7,13 @@
       <el-breadcrumb-item>数据报表</el-breadcrumb-item>
     </el-breadcrumb>
 
-    <el-card>
-        123
-    </el-card>
+    <h1>数据报表</h1>
+    <h5>未开始学习echarts，此功能略过</h5>
   </div>
 </template>
 
 <script>
+
 export default {
   data() {
     return {}
@@ -21,7 +21,6 @@ export default {
 
   created() {},
 
-  methods: {},
 }
 </script>
 
