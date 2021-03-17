1ef0f42f366504ebafb48e7b5f35421cc5424822
孤云 <1946306647@qq.com>
2021-03-17 11:09:49 +0800

    自定制首页内容

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
-
 module.exports = {
     chainWebpack: config => {
         // 发布模式
@@ -13,11 +12,20 @@ module.exports = {
                 nprogress: 'NProgress',
                 'vue-quill-editor': 'VueQuillEditor'
             })
+            config.plugin('html').tap(args => {
+                args[0].isProd = true
+                return args
+            })
         })
 
         // 开发模式
         config.when(process.env.NODE_ENV === 'development', config => {
             config.entry('app').clear().add('./src/main-dev.js')
+
+            config.plugin('html').tap(args => {
+                args[0].isProd = false
+                return args
+            })
         })
     }
 }
