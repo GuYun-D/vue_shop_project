f7c8963c9c7baac4b4b6f5f65d22f94ac0ceaf37
孤云 <1946306647@qq.com>
2021-03-17 10:39:02 +0800

    使用chainWebpack自定义打包入口

---
+++ src/main-prod.js
@@ -0,0 +1,73 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import './plugins/element.js'
+
+// 导入全局样式表
+import './assets/css/global.css'
+
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+
+// 导入axios
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+
+// 导入树形插件
+import TreeTable from 'vue-table-with-tree-grid'
+
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// 导入富文本编辑器的样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+// 全局注册
+Vue.use(VueQuillEditor)
+
+// 导入进度条包
+import Nprogress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+// 在request拦截器中，展示进度条
+axios.interceptors.request.use(config => {
+    Nprogress.start()
+    console.log(config);
+
+    // 自定义的Authorization
+    config.headers.Authorization = window.sessionStorage.getItem('token')
+    // 在最后必须return config
+    return config
+})
+// 在response拦截其中隐藏进度条
+axios.interceptors.response.use(config => {
+    Nprogress.done()
+    return config
+})
+
+Vue.prototype.$http = axios;
+
+Vue.config.productionTip = false
+
+// 全局注册树形组件
+Vue.component('tree-table', TreeTable)
+
+// 自定义格式化时间的全局过滤器
+Vue.filter('dateFormat', function (originVal) {
+    const dt = new Date(originVal)
+
+    const y = dt.getFullYear()
+    const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+    const d = (dt.getDate() + '').padStart(2, '0')
+
+    const hh = (dt.getHours() + '').padStart(2, '0')
+    const mm = (dt.getMinutes() + '').padStart(2, '0')
+    const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+    return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
+
+new Vue({
+    router,
+    render: h => h(App)
+}).$mount('#app')
